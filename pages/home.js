03049a5f297268cb5f24e1f391cb9e021a06a0af
Dylan Pierce <me@dylanjpierce.com>
2021-03-31 15:46:25 -0400

    Adding styling too

--- pages/home.js
+++ pages/home.js
@@ -1,8 +1,9 @@
 import React, { useEffect, useState } from "react";
-import { useApi } from 'shopify-nextjs-toolbox';
+import { useApi, useShopOrigin } from 'shopify-nextjs-toolbox';
 
 
 export default function Home() {
+  const shopName = useShopOrigin();
   const api = useApi();
   const [response, setResponse] = useState(false);
 
@@ -13,14 +14,31 @@ export default function Home() {
       setResponse(res.data);
     })
     .catch((res) => {
-      debugger;
       console.log(res);
     });
   }, []);
 
   return (
-    <>
-      {JSON.stringify(response, null, 2)}
-    </>
+    <div className="container">
+      <div className="card">
+        <h2>Current Decoded Session Token</h2>
+        <p>
+          This is the decoded session token that was sent to the server after the OAuth handshake finished.
+        </p>
+        <p>
+          You can use the backend middleware <code>withSessionToken</code> to verify the API request came from the currently logged in shop 
+        </p>
+        <p>
+          Wrap your API route with <code>withSessionToken</code> to access the shop's origin (a.k.a the shop's name in <code>shop-name.myshopify.com</code> format) in the backend.
+        </p>
+        <pre>
+          {JSON.stringify(response, null, 4)}
+        </pre>
+      </div>
+      <div className="card">
+        <h2>Currently logged in as</h2>
+        <code>{shopName}</code>
+      </div>
+    </div>
   )
 }
