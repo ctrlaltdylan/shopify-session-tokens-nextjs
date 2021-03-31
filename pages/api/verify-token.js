ca3b2f0da6f2c4e2e92bd588a05966d6c885a205
Dylan Pierce <me@dylanjpierce.com>
2021-03-31 13:13:39 -0400

    Fixing misnamed variable

--- pages/api/verify-token.js
+++ pages/api/verify-token.js
@@ -2,7 +2,7 @@ import { withSessionToken } from "shopify-nextjs-toolbox";
 
 const handler = async (req, res) => {
   res.statusCode = 200
-  res.json(req.session_token)
+  res.json(req.sessionToken)
 }
 
 export default withSessionToken(handler);
