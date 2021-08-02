a2679bb6428bcef9d4a2f71d270827d611b87321
Dylan Pierce <me@dylanjpierce.com>
2021-08-01 22:58:02 -0400

    v2 support

--- pages/_app.js
+++ pages/_app.js
@@ -1,34 +1,23 @@
-import React from 'react';
-import '../styles/globals.css'
+import React from "react";
+import "../styles/globals.css";
+import { ShopifyAppBridgeProvider } from "shopify-nextjs-toolbox";
+// import ShopifyAppBridgeProvider from "../components/ShopifyAppBridgeProvider";
 import { Provider } from "@shopify/app-bridge-react";
-import { useShopOrigin } from 'shopify-nextjs-toolbox';
-import enTranslations from '@shopify/polaris/locales/en.json';
-import { AppProvider } from '@shopify/polaris';
-
+import { useHost } from "shopify-nextjs-toolbox";
+import { useShopOrigin } from "shopify-nextjs-toolbox";
+import enTranslations from "@shopify/polaris/locales/en.json";
+import { AppProvider } from "@shopify/polaris";
 
 function MyApp({ Component, pageProps }) {
-  const shopOrigin = useShopOrigin();
-  if (typeof window == "undefined" || !window.location || !shopOrigin) {
-    return (
-      <></>
-    );
-  }
-
-
-
-  const config = {
-    apiKey: process.env.NEXT_PUBLIC_SHOPIFY_API_PUBLIC_KEY,
-    forceRedirect: true,
-    shopOrigin
-  };
-
+  // The ShopifyAppBridgeProvider abstracts starting the OAuth process
+  //   it will automatically redirect unauthenticated users to your `/api/auth.js` route
   return (
-    <Provider config={config}>
+    <ShopifyAppBridgeProvider Component={Component} pageProps={pageProps}>
       <AppProvider i18n={enTranslations}>
-        <Component {...pageProps} /> 
+        <Component {...pageProps} />
       </AppProvider>
-    </Provider>
+    </ShopifyAppBridgeProvider>
   );
 }
 
-export default MyApp
+export default MyApp;
--- pages/index.js
+++ pages/index.js
@@ -1,32 +1,9 @@
 import React, { useEffect } from "react";
-import qs from "querystring";
-import axios from 'axios';
+import { useOAuth } from "shopify-nextjs-toolbox";
 
+export default function Index() {
+  useOAuth();
 
-export default function Home() {
-  // page context params
-  useEffect(() => {
-    if (typeof window !== "undefined" && window.location) {
-      const query = qs.parse(window.location.search);
-      axios
-        .post("/api/auth", {
-          query: query,
-        })
-        .then((response) => {
-          if (response.data.redirectTo) {
-            if (window.parent) {
-              window.parent.location.href = response.data.redirectTo;
-            } else {
-              window.location.href = response.data.redirectTo;
-            }
-          }
-        })
-        .catch(function (error) {
-          // handle error
-          console.error(error);
-        });
-    }
-  }, []);
-
-  return (<>Loading...</>);
+  // replace this with your jazzy loading icon animation
+  return <>Loading...</>;
 }
